945f3b8b0ee24e39d2cada978dbc115d0bb6747c
Muhammad Aulia Rahman <meehchannel7@gmail.com>
2021-09-15 05:16:49 +0700

    1.0
    Tugas 3

---
+++ Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+    public AudioClip jump;
+    private AudioSource audioPlayer;
+    public AudioClip scoreHighlight;
+
+    public void PlayScoreHighlight()
+    {
+        audioPlayer.PlayOneShot(scoreHighlight);
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    [Header("Score Highlight")]
+    public int scoreHighlightRange;
+    public CharacterSoundController sound;
+    private int currentScore = 0;
+    private int lastScoreHighlight = 0;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //reset
+        currentScore = 0;
+        lastScoreHighlight = 0;
+    }
+    
+    public float GetCurrentScore()
+    {
+        return currentScore;
+
+    }
+
+    public void IncreaseCurrentScore(int increment)
+    {
+        currentScore += increment;
+        if (currentScore - lastScoreHighlight > scoreHighlightRange)
+        {
+            sound.PlayScoreHighlight();
+            lastScoreHighlight += scoreHighlightRange;
+        }
+    }
+    
+    public void FinishScoring()
+    {
+        //set highscore
+        if(currentScore > ScoreData.highScore)
+        {
+            ScoreData.highScore = currentScore;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
